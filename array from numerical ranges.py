23bb26c369a9ca37c776d6539feec20cad3fdbbd
codingrad1 <94353288+codingrad1@users.noreply.github.com>
2021-11-17 13:10:43 +0530

    Update array from numerical ranges.py

--- array from numerical ranges.py
+++ array from numerical ranges.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[28]:
-
+# Example 1: numpy.arange function
 
 #arange function
 import numpy as np 
 x = np.arange(6) 
 print (x)
-
-
-# In[29]:
 
 
 import numpy as np 
@@ -19,16 +12,10 @@ x = np.arange(6, dtype = float)
 print (x)
 
 
-# In[30]:
-
-
 # start and stop parameters set 
 import numpy as np 
 x = np.arange(10,30,2) 
 print (x)
-
-
-# In[31]:
 
 
 import numpy as np  
@@ -36,16 +23,12 @@ a = np.arange(5,50,2,int)
 print("The array over the given range is ",a) 
 
 
-# In[32]:
-
+# Example 2: numpy.linspace function
 
 #linspace function
 import numpy as np  
 x = np.linspace(10, 30, 5)  
 print("The array over the given range is ",x)  
-
-
-# In[33]:
 
 
 # endpoint set to false 
@@ -54,28 +37,20 @@ x = np.linspace(10,30, 5, endpoint = False)
 print (x)
 
 
-# In[34]:
-
-
 # find retstep value 
 import numpy as np 
-
 x = np.linspace(1,3,5, retstep = True) 
 # retstep here is 0.5
 print (x)
 
 
-# In[35]:
-
+Example 3: numpy.logspace function
 
 #logspace function
 import numpy as np 
 # default base is 10 
 a = np.logspace(10, 20, num = 10) 
 print (a)
-
-
-# In[36]:
 
 
 # set base of log space to 2 and endpoint is true
