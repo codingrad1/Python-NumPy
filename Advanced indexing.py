a5a060b04b174990cf45abb79ddb6339b5dbb1e7
codingrad1 <94353288+codingrad1@users.noreply.github.com>
2021-11-18 18:46:18 +0530

    Update Advanced indexing.py

--- Advanced indexing.py
+++ Advanced indexing.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# Example 1:
 # Integer indexing 
 import numpy as np 
-
 x = np.array([[1, 2], [3, 4], [5, 6],[7,8]]) 
 print ("The array used for Integer Indexing is:")
 print(x)
@@ -16,9 +10,8 @@ print("The Output is:")
 print(y) 
 
 
-# In[2]:
 
-
+# Example 2:
 # ndarray object containing corner elements.
 import numpy as np 
 x = np.array([[1,2,3], [4,6,8], [10,20,30], [92, 93, 94]]) 
@@ -37,25 +30,8 @@ print ('The corner elements of the array are:')
 print (y)
 
 
-# In[3]:
 
-
-# Booelan Indedxing
-import numpy as np 
-x = np.array([[ 0,  1,  2],[ 3,  4,  5],[ 6,  7,  8],[ 9, 10, 11]]) 
-
-print ('The array is:') 
-print (x) 
-print ('\n')  
-
-# Now we will print the items greater than 5 
-print ('The items greater than 5 are:') 
-print (x[x > 6])
-
-
-# In[4]:
-
-
+# Example 3:
 #Boolean indexing 
 import numpy as np 
 x = np.array([[ 0,  1,  2],[ 3,  4,  5],[ 6,  7,  8],[ 9, 10, 11]]) 
@@ -69,9 +45,8 @@ print ('The items greater than 4 are:')
 print (x[x > 4])
 
 
-# In[5]:
 
-
+#Example 4:
 # Using NAN 
 import numpy as np 
 
@@ -81,10 +56,8 @@ print("After omitting NaN the output array is :")
 print (a[~np.isnan(a)])
 
 
-# In[6]:
 
-
+# Example 5:
 import numpy as np 
 a = np.array([1, 2.5+6j, 5, 3+5j]) 
 print (a[np.iscomplex(a)])
-
